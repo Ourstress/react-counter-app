45a1b85ae55a781b7f670c0400a78248dfb46f4a
SG <sgng1@e.ntu.edu.sg>
2018-10-01 10:03:05 +0100

    completed exercise

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import "./App.css";
 import Counter from "./Counter";
 
 class App extends Component {
+  jumpstarters = [{name:"Xinfang",value:1}, {name:"Sheldon",value:5}, {name:"Sebastian",value:2}, {name:"Brian",value:6}, {name:"Huimin",value:5}, {name:"Calvin",value:3}, {name:"Shun",value:4}, {name:"Tim",value:2}, {name:"Delph",value:7}, {name:"Kaixin",value:2}]
   render() {
     return (
-      <div className="App">
-        <Counter />
+      <div className="App center">
+        {this.jumpstarters.map((person)=><Counter name={person.name} value={person.value} />)}
       </div>
     );
   }
--- src/Counter.js
+++ src/Counter.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { Component } from 'react';
 
-const Counter = () => {
-  return (
-    <div>
-      <h1 className="big">Value</h1>
-    </div>
-  );
-};
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: this.props.value
+    };  
+  }
+  increment = () => {
+    this.setState({value:this.state.value+1})
+  }
+  decrease = () => {
+    this.setState({value:this.state.value-1})
+  }
+  render() {
+    return (
+      <div className="border border-warning rounded">
+        <h1>{this.props.name}</h1>
+        <h3 className={(this.state.value===0)?"bg-secondary": (this.state.value>0)?"bg-primary":"bg-danger"}>{(this.state.value===0)?"zero": (this.state.value>0)? "positive":"negative"}</h3>
+        <h1 className="big">{this.state.value}</h1>
+        <button className="btn btn-primary btn-lg m-2" onClick={this.increment}>+</button>
+        <button className="btn btn-danger btn-lg" onClick={this.decrease}>-</button>
+      </div>
+    );
+  }
+}
 
 export default Counter;
